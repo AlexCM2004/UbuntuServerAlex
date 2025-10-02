7a1b7e226bd97bc24ff3b24a59615466bc7275e9
AlexCM <pokealex2.carrasco@gmail.com>
2025-10-01 19:42:32 -0600

    Un chingo de cambios otra vez

--- proyecto_alex/alex_app/urls.py
+++ proyecto_alex/alex_app/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:nombre>", views.saludo, name="saludo")
+    path("<str:nombre>", views.saludo, name="saludo"),
+    path("tasks", views.tasks_index, name="tasks_index"),
+    path("tasks/add", views.tasks_add, name="tasks_add"),
+    path("admin-tasks", views.task_admin_list, name="tasks_admin_list"),
+    path("menu", views.index2, name="index2")
 ]
--- proyecto_alex/alex_app/views.py
+++ proyecto_alex/alex_app/views.py
@@ -9,3 +9,23 @@ def index(request):
 
 def saludo (request, nombre):
 	return render(request, "alex_app/saludo.html",{"nombre" : nombre.capitalize()})
+
+tasks = ["foo", "bar", "baz"]
+
+def tasks_index (request):
+	return render(request, "myapp/tasks_index.html", {
+		"tasks": tasks})
+
+def tasks_add (request):
+	if request.method == "POST":
+		task = request.POST.get("task")
+		if task:
+			task.append(task)
+		return HttpResponseRedirect(reverse("tasks_index"))
+	return render(request, "myapp/task_add.html")
+
+def tasks_admin_list (request):
+	task = Task.objects.all().order_by("created at")
+	return render(request, "myapp/task_admin_list.html", {
+		"tasks": task
+	})
